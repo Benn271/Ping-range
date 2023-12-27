d7f24fc6796748c0be364071294d3f5e68cbe14d
Benn271 <137446774+Benn271@users.noreply.github.com>
2023-12-27 14:36:11 -0600

    Add the code
    Add the code

---
+++ python_ping_automation/checkip.py
@@ -0,0 +1,14 @@
+import os
+import ipaddress
+import sys
+
+
+def check(ip):
+
+    try:
+        ip_obj = ipaddress.ip_address(ip)
+        return True
+    except ValueError:
+        print(f"ERROR: {ip} is not a valid IP address!")
+        sys.exit()
+        
